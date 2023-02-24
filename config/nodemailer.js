bf6bc6e5d47361e6a1efc4a2fccc34f7cb0e233b
Solomon Olatunji <realsolomon07@gmail.com>
2023-02-24 09:43:09 +0100

    ..

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,7 +4,7 @@ const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
+  service: "gmail",
   auth: {
     user: email,
     pass,
